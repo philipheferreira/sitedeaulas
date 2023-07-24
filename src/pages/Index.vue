e8d6153f64fc919d810228935a7cb463bf4acc7c
philipheferreira <philipheferreira@gmail.com>
2023-07-24 11:41:35 -0300

    update: info and site

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,18 +1,18 @@
 <template>
   <q-page class="">
-    <div class="column items-center">
-      <img alt="Quasar logo" src="https://www.unijui.edu.br/arquivos/tinyMCE4/comunica1/rotina%20de%20estudos%202023.png"
-        style="height: 300px; width: 300px;" class="q-ma-md" />
-      <p>Professor: Milton Oliveira</p>
-    </div>
-    <div class="info q-ma-md">
-      <h5 class="text-center">sobre</h5>
+    <div class="info q-ma-lg">
+      <h5 class="text-center">Professor Milton Oliveira</h5>
+      <div class="column items-center">
+        <img alt="Quasar logo"
+          src="https://www.unijui.edu.br/arquivos/tinyMCE4/comunica1/rotina%20de%20estudos%202023.png"
+          style="height: 300px; width: 300px;" class="q-ma-md" />
+      </div>
       <p>Estudo a um tempo como a matemática afeta a aprendizagem e sua importância dentro do estudo geral humano.
         Trabalho com meus alunos dentro da perspectiva de auxiliá-los no contato com a disciplina e aprimoramento pessoal
       </p>
       <p><strong>Formação:</strong> Licenciatura plena em matemática na UFPA em 2019</p>
     </div>
-    <div class="q-pa-md">
+    <div class="q-pa-lg">
       <q-table title="Disponibilidade de Horario" :rows="rows" :columns="columns" row-key="name" hide-bottom />
     </div>
   </q-page>
